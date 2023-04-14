96657ff479b5f894838ed02ff824332baa37fd90
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-04-14 12:14:26 +0300

    admin dashboard

--- virtual-school/src/App.js
+++ virtual-school/src/App.js
@@ -1,29 +1,33 @@
 // import logo from "./logo.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Assessment from "./components/Assessment";
-import Chat from "./components/Chat";
-import Dashboard from "./components/Dashboard";
-import Exam from "./components/Exam";
-import Landing from "./components/Landing";
-import Resource from "./components/Resource";
-import Result from "./components/Result";
-import Test from "./components/Test";
+import Assessment from "./components/Pages/Assessment";
+import Chat from "./components/Pages/Chat";
+import Dashboard from "./components/Pages/Dashboard";
+import Exam from "./components/Pages/Exam";
+import Landing from "./components/Pages/Landing";
+import Resource from "./components/Pages/Resource";
+import Result from "./components/Pages/Result";
+import Test from "./components/Pages/Test";
+import Admin from "./components/Pages/Admin";
+
+console.log(Dashboard);
 
 function App() {
   return (
     <BrowserRouter>
-      <Dashboard>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/resources" element={<Resource />} />
-          <Route path="/exams" element={<Exam />} />
-          <Route path="/test" element={<Test/>} />
-          <Route path="/assessment" element={<Assessment />} />
-          <Route path="/results" element={<Result />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </Dashboard>
+      {/* <Dashboard> */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/resources" element={<Resource />} />
+        <Route path="/exams" element={<Exam />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/assessment" element={<Assessment />} />
+        <Route path="/results" element={<Result />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+      {/* </Dashboard> */}
     </BrowserRouter>
   );
 }
