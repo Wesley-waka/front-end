e3b0b3481f00ce38847e5a7dbaea7068d4ded573
Rawllings Kiprop <rutojr01@gmail.com>
2023-04-18 12:50:22 +0300

    landing page

--- virtual-school/src/components/Pages/Landing.js
+++ virtual-school/src/components/Pages/Landing.js
@@ -2,6 +2,9 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 import { NavLink } from "react-router-dom";
 import "../Pages/Dashboard.css";
+import { MdOutlineLogin } from "react-icons/md";
+import { GoSignIn } from "react-icons/go";
+
 
 function Landing() {
   return (
@@ -55,67 +58,34 @@ function Landing() {
             </h1>
           </NavLink>
           <ul className="space-y-2 font-medium">
-            {/* <li>
-              <NavLink
-                to="/"
-                className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-black dark:hover:bg-gray-700"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="w-6 h-6 text-white transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                </svg>
-                <span className="ml-3 text-2xl text-start">Dashboard</span>
-              </NavLink>
-            </li> */}
+           
 
             <li>
               <NavLink
                 to="/login"
                 className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-black dark:hover:bg-gray-700"
               >
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-white  transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="flex-1 ml-3 text-2xl text-start">Sign In</span>
+                 <div className="text-xl">
+                    <MdOutlineLogin />
+                  </div>
+                <span className="flex-1 ml-3 text-2xl text-start">Login</span>
               </NavLink>
             </li>
+
             <li>
               <NavLink
                 className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-black dark:hover:bg-gray-700"
                 to="/signUp"
               >
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+               <div className="text-xl">
+                    <GoSignIn />
+                  </div>
                 <span className="flex-1 ml-3 text-2xl text-start">Sign Up</span>
               </NavLink>
             </li>
+            <div style={{paddingTop: "370px" }}>
+              <hr className="border-4 items-center pr-10"></hr>
+            </div>
           </ul>
         </div>
       </aside>
