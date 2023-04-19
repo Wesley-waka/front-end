b0f5fabb61d0accd83359f148d4ec9cb77d2824f
Javoh <javan.mugambi@student.moringaschool.com>
2023-04-19 19:30:35 +0300

    educator/examschedule

--- virtual-school/src/App.js
+++ virtual-school/src/App.js
@@ -9,13 +9,13 @@ import Result from "./components/Pages/Result";
 import Admin from "./components/Pages/Admin";
 import StudentDashboard from "./components/Pages/StudentDashboard";
 import Login from "./components/Pages/Login";
-import SignUp from "./components/Pages/SignUp";
 import AuthProvider from "./components/Pages/AuthContext";
 import SchoolEntry from "./components/Pages/SchoolEntry";
 import CourseEntry from "./components/Pages/CourseEntry";
 import EducatorEntry from "./components/Pages/EducatorEntry";
 import StudentEntry from "./components/Pages/StudentEntry";
-
+import Educator from "./components/Pages/Educator";
+import Examschedule from "./components/Pages/Examschedule";
 
 
 function App() {
@@ -25,19 +25,20 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Landing />} />
-        <Route path="/admin/student" element={<StudentEntry />} />
-        <Route path="/admin/course" element={<CourseEntry />} />
-        <Route path="/admin/educator" element={<EducatorEntry />} />
-
+          <Route path="/admin/student" element={<StudentEntry />} />
+          <Route path="/admin/course" element={<CourseEntry />} />
+          <Route path="/admin/educator" element={<EducatorEntry />} />
           <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/admin/school" element={<SchoolEntry />} />
+          <Route path="/admin/school" element={<SchoolEntry />} />
           <Route path="/student/resources" element={<Resource />} />
           <Route path="student/exams" element={<Exam />} />
           <Route path="student/results" element={<Result />} />
           <Route path="student/chat" element={<Chat />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/educator" element={<Educator/>} />
+          <Route path="/educator/examschedule" element={<Examschedule/>} />
+          
         </Routes>
       </AuthProvider>
     </BrowserRouter>
